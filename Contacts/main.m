a47a2701926b7bc5d7e8447131d18c01ad1780de
Michael Berkovich <michael@geni.com>
2012-01-10 14:42:16 -0800

    Initial Commit

---
+++ Contacts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Contacts
+//
+//  Created by Michael Berkovich on 1/10/12.
+//  Copyright (c) 2012 Geni.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
