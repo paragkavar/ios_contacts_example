46ddba722da6a02e5de9181e29acf43fb5ef9b81
Michael Berkovich <michael@geni.com>
2012-01-10 22:28:39 -0800

    minor touches

--- Contacts/FacebookImportOperation.m
+++ Contacts/FacebookImportOperation.m
@@ -1,10 +1,19 @@
-//
-//  FacebookImportOperation.m
-//  BirthdayCalendar
-//
-//  Created by Michael Berkovich on 4/3/11.
-//  Copyright 2011 MobTouch, Inc. . All rights reserved.
-//
+/*
+ * Copyright 2011-2012 OAuthX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 
 #import "FacebookImportOperation.h"
 #import "AppDelegate.h"
